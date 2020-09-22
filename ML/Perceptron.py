a4cdf1aa36c38d0555a3d23044796ddd52878a33
Matthew Krohn <matthew.d.krohn@gmail.com>
2020-09-22 07:18:35 -0600

    Added docstrings and comments from pdf

--- ML/Perceptron.py
+++ ML/Perceptron.py
@@ -4,11 +4,55 @@ import pandas as pd, numpy as np, matplotlib.pyplot as plt
 class Perceptron:
     def __init__(self, learning_rate, iterations):
         assert 0 < learning_rate < 1
-        self.learning_rate = learning_rate
-        self.iterations = iterations
+        self.rate = learning_rate
+        self.niter = iterations
+        self.errors = []
 
-    def print_vals(self):
-        print(self.learning_rate, self.iterations)
+    def fit(self, X, y):
+        """
+        Fit training data
 
-    def fit(self):
+        :param X Training vectors, X.shape : [#samples, #features]
+        :param y Target values, y.shape : [#samples]
+        """
+
+        # weights: create a weights array of right size and
+        # initialize elements to zero
+
+        # Number of misclassifications, creates an array
+        # to hold the number of misclassifications
+
+        # main loop to fit the data to the labels
+
+        # for i in range(self.niter):
+
+            # set iteration error to zero
+            # loop over all the objects in X and corresponding y element
+
+            # for xi, target in zip(X, y):
+
+                # calculate the needed (delta_w) update from prev step
+                # delta_w = rate * ( target - prediction current object )
+
+                # calculate what the current object will add to the weight
+
+                # set the bias to be the current delta_w
+
+                # increase the iteration error if delta_w != 0
+
+            # Update the miscalssification array with # of errors in iteration
+
+        # return self
+        pass
+
+    def net_input(self, X):
+        """
+        Calculate net input
+        """
+        pass
+
+    def predict(self, X):
+        """
+        Return class label after unit step
+        """
         pass
